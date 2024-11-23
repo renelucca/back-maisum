d01609b1924de70008aef8b6a53b23428f2c187e
renelucca <luccarene91@gmail.com>
2024-11-23 11:44:43 -0300

    mensagem hello world p teste

--- backend/src/App.js
+++ backend/src/App.js
@@ -23,3 +23,7 @@ app.use("/usuarios", usuarioRotas);
  app.listen(5000, () => {
   console.log("Servidor rodando na porta 5000!");
 });
+
+app.get("/", (req, res) => {
+  return res.json("Hello World");
+});
